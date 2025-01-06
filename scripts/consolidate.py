542fdade68020e00577f3831baaeb570a1bf8b34
Jan Tagscherer <jan@jans-work-macbook.local>
2025-01-06 11:01:20 +0100

    Use copyfile instead of copy

--- scripts/consolidate.py
+++ scripts/consolidate.py
@@ -48,7 +48,7 @@ def consolidate_models(source_dirs: Sequence[Path], target_dir: Path, ckpt: str)
         assert len(found_models) == 1, f"Found wrong number of models, {found_models}"
         model_path = found_models[0]
         assert f"fold{fold}" in str(model_path.parent.stem), f"Expected fold {fold} but found {model_path}"
-        shutil.copy(model_path, target_dir / f"model_fold{fold}.ckpt")
+        shutil.copyfile(model_path, target_dir / f"model_fold{fold}.ckpt")
 
 
 def consolidate_predictions(
@@ -76,7 +76,7 @@ def consolidate_predictions(
     pred_dir.mkdir(parents=True, exist_ok=True)
     for source_dir in source_dirs:
         for p in [p for p in (source_dir / postfix).iterdir() if p.is_file()]:
-            shutil.copy(p, pred_dir)
+            shutil.copyfile(p, pred_dir / os.path.basename(p))
 
 
 @env_guard
@@ -166,8 +166,8 @@ def main():
         consolidate=consolidate,
         )
 
-    shutil.copy(training_dirs[0] / "plan.pkl", target_dir)
-    shutil.copy(training_dirs[0] / "config.yaml", target_dir)
+    shutil.copyfile(training_dirs[0] / "plan.pkl", target_dir / "plan.pkl")
+    shutil.copyfile(training_dirs[0] / "config.yaml", target_dir / "config.yaml")
 
     # invoke new parameter sweeps
     cfg = OmegaConf.load(str(target_dir / "config.yaml"))
